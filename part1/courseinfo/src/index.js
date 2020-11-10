f5d04d10b2ef7f96e41e7aeb3755b12eb06ae9a6
Dan Chang <dmagma@hotmail.com>
2020-11-10 12:13:49 -0800

    Completed exercises 1.1-1.5

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const Header = props => {
+  return (
+      <div>
+          <h1>{props.course}</h1>
+      </div>
+  )
+};
+
+const Part = props => {
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = props => {
+  return (
+    <div>
+      <p>
+        Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    </div>
+  )
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Part part={course.parts[0].name} exercises={course.parts[0].exercises}/>
+      <Part part={course.parts[1].name} exercises={course.parts[1].exercises}/>
+      <Part part={course.parts[2].name} exercises={course.parts[2].exercises}/>
+      <Total exercises1={course.parts[0].exercises} exercises2={course.parts[1].exercises} exercises3={course.parts[2].exercises}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
